21bfae9064257233c74d8a55824be03bdfb020d2
Yonathan K <ykebede2@umbc.edu>
2020-06-21 03:21:30 -0400

    Complete first CV project

--- face_detection/face_detect.py
+++ face_detection/face_detect.py
@@ -1,1 +1,28 @@
 import cv2 as cv
+import time
+
+video = cv.VideoCapture(0)
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+while True:
+    
+    time.sleep(0.2)
+
+    ret, frame = video.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+
+    for (x,y,w,h) in faces:
+        frame = cv.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+
+    cv.imshow('Capturing', frame)
+    key = cv.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+
+video.release()
+cv.destroyAllWindows
